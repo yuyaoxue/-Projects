d0837652b787d07fcb54b2d85dbc26c5d7a33173
Yuyao <972934652@qq.com>
2018-06-08 00:19:59 +0800

    提交进度

--- FlapyBirdClient/FlapyBird_Toys/Assets/Scripts/Managers/GameMananager.cs
+++ FlapyBirdClient/FlapyBird_Toys/Assets/Scripts/Managers/GameMananager.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameMananager : MonoBehaviour {
-
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            NextMovePosition.x = _bird.transform.localPosition.x;
+            NextMovePosition.y= _bird.transform.localPosition.y+ 1f;
+            IsKeyDown = true;
+        }
+        if(IsKeyDown)
+        {
+            _bird.transform.localPosition = Vector3.Lerp(_bird.transform.localPosition, NextMovePosition, speed);
+            _bird.transform.GetComponent<Rigidbody2D>().position = _bird.transform.localPosition;
+        }
+    }
+    private bool IsKeyDown = false;
+    private Vector3 NextMovePosition = Vector3.zero;
+    private const float speed = 0.1f;
+    [SerializeField]
+    private GameObject _bird;
 }
